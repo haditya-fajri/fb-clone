602b5a3dca24aafdd3eb553848a4d51cfdc59a67
Haditya <hadityafajri.hf@gmail.com>
2021-04-30 14:38:05 +0700

    improve

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDx-b3ly-__vjBNvMqTSKd-NNrUzMejnVA",
+    authDomain: "fb-clone-1b14c.firebaseapp.com",
+    databaseURL: "https://fb-clone-1b14c.firebaseio.com",
+    projectId: "fb-clone-1b14c",
+    storageBucket: "fb-clone-1b14c.appspot.com",
+    messagingSenderId: "624517242300",
+    appId: "1:624517242300:web:64fd814c38473914505fb9"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
